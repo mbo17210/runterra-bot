5c4192813a22634ebfc2d4e1eb40c758a9b555ff
mbo17210 <mbo17210@gmail.com>
2022-02-06 12:00:51 -0800

    Finished card loader

--- card.py
+++ card.py
@@ -1,23 +1,25 @@
+from enum import Enum
+
 class Card:
-    def __init__(self, cost, region):
+    def __init__(self, cost, name):
         self.cost = cost
-        self.region = region
+        self.name = name
 
 class Minion(Card):
     #Effect is from class effect.
-    def __init__(self, cost, attack, defense, effect, tag),:
-        super().__init__(cost)
+    def __init__(self, cost, name, attack, health, tag = None, effects = None):
+        super().__init__(cost, name)
         self.attack = attack
-        self.defense = defense
-        self.effect = effect
+        self.health = health
         self.tag = tag
+        self.effects = effects
 
 class Spell(Card):
     #Speed is from SpellSpeed Enum. Effect is of class effect.
-    def __init__(self, cost, speed, effect):
-        super().__init__(cost)
+    def __init__(self, cost, name, speed, effects):
+        super().__init__(cost, name)
         self.speed = speed
-        self.effect = effect
+        self.effects = effects
 
 class SpellSpeed(Enum):
     BURST = 3
@@ -28,9 +30,17 @@ class SpellSpeed(Enum):
 class Effect:
     #If num_targets = 0, that implies the card doesn't specifically target something
     #Hit nexus: 0 = no, 1 = possible, 2 = must (eg decimate)
-    def __init__(self, targets, num_targets, damage, atk_buff, def_buff, hit_nexus)
+    def __init__(self, targets, num_targets, damage, atk_buff, def_buff, buff_target, hit_nexus):
+        self.targets = targets
+        self.num_targets = num_targets
+        self.damage = damage
+        self.atk_buff = atk_buff
+        self.def_buff = def_buff
+        self.buff_target = buff_target
+        self.hit_nexus = hit_nexus
 
 class Targets(Enum):
     ENEMIES = 1
     ALLIES = 2
     ALL = 3
+    SELF = 4
---
+++ loader.py
@@ -0,0 +1,72 @@
+import json
+from card import Minion, Spell, Effect
+
+#Change if library location moves
+library_location = r'card/card_library.json'
+
+#Load JSON
+f = open(library_location)
+card_data = json.load(f)
+
+card_library = {}
+
+#Feed in one effect dictionary from card loader and load it as an effect
+def effectLoader(effect):
+    targets = 0 if "targets" not in effect else effect["targets"]
+    num_targets = 0 if "num_targets" not in effect else effect["num_targets"]
+    damage = 0 if "damage" not in effect else effect["damage"]
+    atk_buff = 0 if "atk_buff" not in effect else effect["atk_buff"]
+    def_buff = 0 if "def_buff" not in effect else effect["def_buff"]
+    buff_target = None if "buff_target" not in effect else effect["buff_target"]
+    hit_nexus = 0 if "hit_nexus" not in effect else effect["hit_nexus"]
+    return Effect(targets, num_targets, damage, atk_buff, def_buff, buff_target, hit_nexus)
+
+#Loads minion w/ info from library
+def minionLoader(minion, effects, name = None):
+    cost = minion["cost"]
+    name = name if name != None else minion["name"]
+    attack = minion["attack"]
+    health = minion["health"]
+    tag = None if "tag" not in minion else minion["tag"]
+    return Minion(cost, name, attack, health, tag, effects)
+
+#Loads spell w/ info from library
+def spellLoader(spell, effects):
+    cost = spell["cost"]
+    name = spell["name"]
+    speed = spell["speed"]
+    return Spell(cost, name, speed, effects)
+    
+
+
+for item in card_data["Champions"]:
+    unleveled_effects = []
+    if "effects" in item["unleveled"]:
+        for effect in item["unleveled"]["effects"]:
+            unleveled_effects.append(effectLoader(effect))
+    unleveled_champ = minionLoader(item["unleveled"], unleveled_effects, item["name"])
+    
+    leveled_effects = []
+    if "effects" in item["leveled"]:
+        for effect in item["leveled"]["effects"]:
+            leveled_effects.append(effectLoader(effect))
+    leveled_champ = minionLoader(item["leveled"], leveled_effects, item["name"])
+
+    card_library[item["name"]] = [unleveled_champ, leveled_champ]
+
+for item in card_data["Minions"]:
+    effects = []
+    if "effects" in item:
+        for effect in item["effects"]:
+            effects.append(effectLoader(effect))
+    card_library[item["name"]] = minionLoader(item, effects)
+
+for item in card_data["Spells"]:
+    effects = []
+    #Spells always have effect
+    for effect in item["effects"]:
+        effects.append(effectLoader(effect))
+    card_library[item["name"]] = spellLoader(item, effects)
+
+for key in card_library:
+    print(key)
